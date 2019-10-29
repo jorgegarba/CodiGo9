967f20074c2cc9cbecf9db97de07b7f1be800856
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-10-28 20:51:14 -0500

    avance lunes

--- Semana5/Dia1/03-ejercicios.js
+++ Semana5/Dia1/03-ejercicios.js
@@ -15,3 +15,26 @@ let rest1 = new Restaurante(n,d);
 // Hacer un menu que:
 // Al ingresar la opcion 1 se agrege un plato, al ingresar la opcion 2, muestre todos los platos, y al ingresar la opcion 3, salga del programa.
 // NOTA: Cada plato debe contener su nombre(string), calorias(int) y precio(float) y si se ingresa otra opcion, indicar que no hay una accion para esa opcion.
+do{
+    opcion= +prompt("Ingrese una opcion:\n1. Agregar un plato.\n2. Mostrar los platos.\n3. Salir.")
+    switch (opcion) {
+        case 1:
+            let platoTemporal ={
+                nombres: prompt("Ingrese el nombre del plato"),
+                calorias: +prompt(`Ingrese las calorias del plato ${this.nombres}`),
+                precio: +prompt(`Ingrese el precio del plato ${this.nombres}`)
+            }
+            rest1.platos.push(platoTemporal);
+            break;
+        case 2:
+            console.log(rest1.platos);
+            
+            break;
+        case 3:
+            alert("Adiocito");
+            break;
+        default:
+            alert("Opcion incorrecta, ingrese de nuevo")
+            break;
+    }
+}while (opcion!=3)
