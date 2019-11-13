e096d2bf49686625087c3e0a0dada26c81215324
Jorge Garnica <jorgegarba@gmail.com>
2019-11-12 23:19:47 -0500

    EJERCICIOS DOM Y BOOTSTRAP

--- Semana7/Dia2/01-ejercicio.js
+++ Semana7/Dia2/01-ejercicio.js
@@ -1,12 +1,31 @@
-import { trabajadores } from './01-datos.js';
+import { losTrabajadores } from './01-datos.js';
+
+let trabajadores = losTrabajadores;
 
 let cuerpoTabla = document.getElementById("cuerpoTabla");
+
+
+let eliminarPorId = (idTrabajador) => {
+
+  let filtrado = trabajadores.filter((trabajador, i) => {
+    if (trabajador.id != idTrabajador) {
+      return trabajador;
+    }
+  });
+
+  trabajadores = filtrado;
+  cuerpoTabla.innerText = "";
+  dibujarTabla();
+}
 
 let dibujarTabla = () => {
 
   trabajadores.forEach((trabajador, i) => {
 
     let tr = document.createElement("tr");
+    tr.ondblclick = () => {
+      console.log("Uf! " + trabajador.nombre);
+    }
 
     let tdId = document.createElement("td");
     tdId.innerText = trabajador.id;
@@ -30,13 +49,23 @@ let dibujarTabla = () => {
 
     let tdImagen = document.createElement("td");
     tdImagen.innerHTML = `<img src="${trabajador.imagen}" width="100"/>`;
-
     tr.appendChild(tdImagen);
 
+    let tdAcciones = document.createElement("td");
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerText = "Eliminar";
+
+    btnEliminar.onclick = function () {
+      console.log("Eliminando al id " + trabajador.id);
+      eliminarPorId(trabajador.id);
+    }
+
+    tdAcciones.appendChild(btnEliminar);
+    tr.appendChild(tdAcciones);
     cuerpoTabla.appendChild(tr);
+  }
 
-
-  })
+  )
 
 }
 
