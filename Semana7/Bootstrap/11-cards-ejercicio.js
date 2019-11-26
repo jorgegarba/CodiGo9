fc25cd0dd71fb566890e1e2f16d606dfa9d58932
Jorge Garnica <jorgegarba@gmail.com>
2019-11-25 21:06:47 -0500

    Solucion ejercicio

--- Semana7/Bootstrap/11-cards-ejercicio.js
+++ Semana7/Bootstrap/11-cards-ejercicio.js
@@ -1,2 +1,41 @@
 import { productos } from './11-card-data.js';
 console.log(productos);
+
+let contenedor = document.getElementById("contenedor");
+let cont = 0;
+
+
+let crearColumnaConProducto = (producto) => {
+  if (producto) {
+    return `<div class="col-md-4">
+            <div class="card">
+              <img src="${producto.imagen}" class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text">
+                    Nombre: ${producto.nombre}
+                  </p>
+                </div>
+              </div>
+          </div>`;
+  } else {
+    return '';
+  }
+}
+
+
+for (let i = 0; i < productos.length; i += 3) {
+
+
+
+  let fila = document.createElement("div");
+  fila.setAttribute("class", "row");
+
+  let prod1 = crearColumnaConProducto(productos[i]);  
+  let prod2 = crearColumnaConProducto(productos[i + 1]);
+  let prod3 = crearColumnaConProducto(productos[i + 2]);
+
+  fila.innerHTML = prod1 + prod2 + prod3;
+  contenedor.appendChild(fila);
+
+}
+
