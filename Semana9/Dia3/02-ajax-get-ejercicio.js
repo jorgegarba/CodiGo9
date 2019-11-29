10fcf9a98116983d277786566679e43775e1a31e
Jorge Garnica <jorgegarba@gmail.com>
2019-11-28 19:09:23 -0500

    Ejercicio AJAX GET

--- Semana9/Dia3/02-ajax-get-ejercicio.js
+++ Semana9/Dia3/02-ajax-get-ejercicio.js
@@ -1,9 +1,34 @@
 let btnTraerGatos = document.getElementById("btnTraerGatos");
 let alertaCargando = document.getElementById("alertaCargando");
+let miTabla = document.getElementById("miTabla");
+let cuerpoTabla = document.getElementById("cuerpoTabla");
+
+
+let dibujarTabla = (json) => {
+  let data = json.data;
+
+  let filas = "";
+
+  data.forEach((usuario) => {
+    let fila = `<tr>
+                    <td>${usuario.id}</td>
+                    <td>${usuario.email}</td>
+                    <td>${usuario.first_name}</td>
+                    <td>${usuario.last_name}</td>
+                    <td>${usuario.avatar}</td>
+                </tr>`;
+    filas = filas + fila;
+  });
+
+  cuerpoTabla.innerHTML = filas;
+  miTabla.removeAttribute("hidden");
+}
 
 let getUsuarios = () => {
   // Mostrando aviso de carga
   alertaCargando.removeAttribute("hidden");
+  // Ocultar la tabla
+  miTabla.setAttribute("hidden", "hidden");
 
   let ajax = new XMLHttpRequest();
   ajax.onreadystatechange = () => {
@@ -11,6 +36,7 @@ let getUsuarios = () => {
       let texto = ajax.responseText;
       let json = JSON.parse(texto);
       console.log(json);
+      dibujarTabla(json);
       alertaCargando.setAttribute("hidden", "hidden");
     }
   }
