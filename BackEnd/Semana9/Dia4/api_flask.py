0e79062d880621d7378bd6c2deac3ecc86ba153c
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-11-29 22:13:19 -0500

    avance backend semana 9

--- BackEnd/Semana9/Dia4/api_flask.py
+++ BackEnd/Semana9/Dia4/api_flask.py
@@ -5,11 +5,53 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 supermercados = [
     {
-        'nombre': 'Plaza Vea',
-        'productos':[
+        'nombre': 'Super',
+        'productos': [
             {
-                'nombre':'Cafe',
-                'precio':24.00
+                'nombre': 'Leche Gloria',
+                'precio': 4.40
+            },
+            {
+                'nombre': 'Ricocan',
+                'precio': 7.40
+            },
+            {
+                'nombre': 'Sapolio',
+                'precio': 3.40
+            },
+            {
+                'nombre': 'Pollo a la brasa',
+                'precio': 19.90
+            }
+        ]
+    },
+    {
+        'nombre': 'Metro',
+        'productos': [
+            {
+                'nombre': 'Leche Gloria',
+                'precio': 4.20
+            },
+            {
+                'nombre': 'Leche de Almendras',
+                'precio': 14.80
+            }
+        ]
+    },
+    {
+        'nombre': 'Franco',
+        'productos': [
+            {
+                'nombre': 'Negrito',
+                'precio': 1.20
+            },
+            {
+                'nombre': 'Arroz Tacuari',
+                'precio': 2.30
+            },
+            {
+                'nombre': 'Inca Cola',
+                'precio': 5.20
             }
         ]
     }
@@ -46,13 +88,54 @@ def traersupers():
 
 # CRUD => Create, Read, Update, Delete
 # Para los metodos GET no se recomienda mandar data por el body, siempre por los parametros
+# EL ENDPOINT ES: http://127.0.0.1:5000
+# localhost = 127.0.0.1
+# ENCODIFICACION URL : https://www.w3schools.com/tags/ref_urlencode.asp
 @app.route('/supermecadoPorNombre/<string:nombresuper>',methods=['GET'])
 def traersuperpornombre(nombresuper):
     print(nombresuper)
     # buscar por el nombre y que me devuelva solo ese nombre
     # PISTA: Usar for
-    return nombresuper
+    for supermercado in supermercados:
+        if supermercado['nombre']==nombresuper:
+            return jsonify(supermercado)
+        print(supermercado['nombre'])
+    return 'No existe ese supermercado'
 
+# AGREGAR UN PRODUCTO A UN SUPERMERCADO SIEMPRE Y CUANDO EXISTA EL SUPERMERCADO
+@app.route('/supermercado/<string:nombresuper>/agregarproducto',
+methods=['POST'])
+def agregar_producto(nombresuper):
+    for supermercado in supermercados:
+        if supermercado['nombre']==nombresuper:
+            # request.get_json sirve para recoger toda la data enviada por el cuerpo (body)
+            data = request.get_json()
+            nuevo_producto = {
+                "nombre": data['nombre'],
+                "precio": data['precio']
+            }
+            supermercado['productos'].append(nuevo_producto)
+            return jsonify(supermercado), 201
+    return jsonify(
+        {
+            'message':'Supermarket not found'
+        }
+    ),418
+
+@app.route('/supermercado/<string:nombre>/productos', methods=['GET'])
+def traer_todos_los_productos_de_un_supermercado(nombre):
+    # funcion que traiga todos los productos de un supermercado
+    for supermercado in supermercados:
+        if supermercado['nombre']==nombre:
+            return jsonify(
+                {
+                    'Productos': supermercado['productos']
+                }
+            )
+    return jsonify({
+            'message':'Supermarket not found'
+        }
+    ),418
 
 # EL MAIN SIEMPRE VA AL FINAL
 if __name__=='__main__':
