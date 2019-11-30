d1099c04bbb0785b07188d98658e16152e443994
Jorge Garnica <jorgegarba@gmail.com>
2019-11-29 19:11:37 -0500

    AJAX POST para el Sr. Campana

---
+++ Semana9/Dia5/01-ajax-post.js
@@ -0,0 +1,11 @@
+let formRegistro = document.getElementById("formRegistro");
+let inputNombre = document.getElementById("inputNombre");
+let selectPuesto = document.getElementById("selectPuesto");
+
+
+
+
+
+formRegistro.onsubmit = () => {
+  
+}
