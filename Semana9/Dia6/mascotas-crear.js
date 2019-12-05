892cc0dcd88f1537c8d748ae9f612353f46480f5
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-12-04 20:20:37 -0500

    avance semana 10 dia 2

--- Semana9/Dia6/mascotas-crear.js
+++ Semana9/Dia6/mascotas-crear.js
@@ -12,6 +12,19 @@ coloresDisponibles.forEach((color) => {
   option.innerText = color.charAt(0).toUpperCase() + color.substring(1);
   selectColores.appendChild(option);
 })
+
+let eliminarColor = (elemento)=>{
+  console.log(elemento);
+  listaColores.removeChild(elemento);
+  console.log(coloresElegidos.indexOf(elemento.id));
+  coloresElegidos.splice(coloresElegidos.indexOf(elemento.id),1);
+  let option = document.createElement("option");
+  option.setAttribute("value",elemento.id);
+  option.innerText = elemento.id.charAt(0).toUpperCase() + elemento.id.substring(1);
+  coloresDisponibles.push(elemento.id);
+  selectColores.appendChild(option);
+}
+
 btnAgregar.onclick = () => {
   let indice = selectColores.selectedIndex;
   let color = document.createElement("li");
@@ -19,6 +32,12 @@ btnAgregar.onclick = () => {
   color.setAttribute("id", coloresDisponibles[indice]);
   color.innerText = coloresDisponibles[indice].charAt(0).toUpperCase() + coloresDisponibles[indice].substring(1);
   coloresElegidos.push(coloresDisponibles[indice]);
+  coloresDisponibles.splice(indice,1);
+  selectColores.remove(selectColores.selectedIndex);
+  console.log(coloresDisponibles);
+  color.ondblclick = (e)=>{
+    eliminarColor(e.target);
+  }
   console.log(coloresElegidos);
   listaColores.appendChild(color);
 }
