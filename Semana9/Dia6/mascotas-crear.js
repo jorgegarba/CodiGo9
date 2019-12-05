24074e09d5575dd15d82d30c5f7d69917f75e7cf
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-12-04 19:33:27 -0500

    avance frontend

--- Semana9/Dia6/mascotas-crear.js
+++ Semana9/Dia6/mascotas-crear.js
@@ -1,11 +1,24 @@
 
 let selectColores = document.getElementById("selectColores");
+let listaColores = document.getElementById("lista-colores");
+let btnAgregar = document.getElementById("btnAgregarColor");
 let coloresDisponibles = ["blanco", "negro", "cafe", "beige", "plomo"];
+let coloresElegidos = []
 
 coloresDisponibles.forEach((color) => {
   let option = document.createElement("option");
-  
+
   option.setAttribute("value", color);
   option.innerText = color.charAt(0).toUpperCase() + color.substring(1);
   selectColores.appendChild(option);
 })
+btnAgregar.onclick = () => {
+  let indice = selectColores.selectedIndex;
+  let color = document.createElement("li");
+  color.setAttribute("class", "list-group-item");
+  color.setAttribute("id", coloresDisponibles[indice]);
+  color.innerText = coloresDisponibles[indice].charAt(0).toUpperCase() + coloresDisponibles[indice].substring(1);
+  coloresElegidos.push(coloresDisponibles[indice]);
+  console.log(coloresElegidos);
+  listaColores.appendChild(color);
+}
