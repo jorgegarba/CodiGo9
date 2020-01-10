65bc2778b0b702d5aed9ad541df922deb5038286
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-01-09 19:01:52 -0500

    avance del avance

---
+++ BackEnd/Semana13/Veterinaria/controllers/horario.py
@@ -0,0 +1,37 @@
+from flask_restful import Resource, reqparse
+from models.horario import HorarioModel
+from models.usuario import UsuarioModel
+
+class HorarioController(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            "fecha_inicio",
+            type=str,
+            required=True,
+            help="Se requiere la fecha de inicio"
+        )
+        parser.add_argument(
+            "fecha_fin",
+            type=str,
+            required=True,
+            help="Se requiere la fecha de fin"
+        )
+        parser.add_argument(
+            "hora_ingreso",
+            type=str,
+            required=True,
+            help="Se requiere la hora de ingreso"
+        )
+        parser.add_argument(
+            "hora_salida",
+            type=str,
+            required=True,
+            help="Se requiere la hora de salida"
+        )
+        parser.add_argument(
+            "usuario",
+            type=int,
+            required=True,
+            help="Se requiere el id de usuario"
+        )
