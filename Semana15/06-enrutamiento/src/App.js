9504d961b9df38911949bb95d682c9c04599fa82
jmontes50 <osmarmontes50@gmail.com>
2020-01-21 18:47:16 -0500

    06- Enrutamiento Update 1

---
+++ Semana15/06-enrutamiento/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+import Header from './components/Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header/>
+      </Fragment>
+    )
+  }
+}
