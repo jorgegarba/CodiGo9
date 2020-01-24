b8c7ccd6b05162bd4ef8bed7ee7764dcb57235a3
Jorge Osmar Montesinos Aramayo <jorge.montesinos@tecsup.edu.pe>
2020-01-23 19:11:06 -0500

    Update 2

--- Semana15/07-pokedex/src/paginas/PokeInfo.js
+++ Semana15/07-pokedex/src/paginas/PokeInfo.js
@@ -3,10 +3,26 @@ import React, { Component } from 'react'
 export default class PokeInfo extends Component {
     constructor(props){
         super(props);
-        console.log(props);
+        // console.log("parametros",props);
         this.state = {
-            poke:this.props.match.params.pokemon
+            poke:this.props.match.params.pokemon,
+            pokemon:{}
         }
+    }
+
+    traerPokemon = async (nombrePokemon) => {
+        let pokeEndPoint = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`;
+
+        let response = await fetch(pokeEndPoint);
+        let json = await response.json();
+        console.log(json);
+        this.setState({
+            pokemon:json
+        })
+    }
+    
+    componentDidMount(){
+        this.traerPokemon(this.state.poke)
     }
     render() {
         
@@ -17,7 +33,7 @@ export default class PokeInfo extends Component {
                        <div className="card shadow">
                            <div className="card-body">
                                <h1 className="display-4 text-center">
-                                   Charmander
+                                   {this.state.pokemon.name}
                                 </h1>
                            </div>
                        </div>
