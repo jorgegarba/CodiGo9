ccc953adf7362d2cdd2dbf23056b5130ceef9910
Jorge Osmar Montesinos Aramayo <jorge.montesinos@tecsup.edu.pe>
2020-01-24 19:25:53 -0500

    Update 2

--- Semana15/01-node/index.js
+++ Semana15/01-node/index.js
@@ -1,1 +1,17 @@
-console.log("Mi app con nodemon");
+//importando express, que es el framework que nos permitre crear aplicaciones de back con JS
+const express = require('express');
+//Router, servira para manejar las rutas de mi servidor web
+const router = express.Router();
+
+//creando un servidor web
+let app = express();
+
+router.get("/api", (request, response) => {
+    response.send("Todo Ok, el servidor esta funcionando");
+})
+
+app.use(router);
+
+app.listen(4000, ()=> {
+    console.log("El servidor Sha esta corriendo en el puerto 4000");
+})
