39c99b48c77f6bec37cfc8e2ee2cc83c6600d675
Osmar Montesinos <osmarmontes50@gmail.com>
2020-02-04 21:08:45 -0500

    Update 1, ya mostrando datos

--- Semana16/ambientes/frontend/src/modulos/admin/screens/pabellones/AdminPabellones.js
+++ Semana16/ambientes/frontend/src/modulos/admin/screens/pabellones/AdminPabellones.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
-import { URL_BACKEND } from './../../../../environments/environments';
-import AdminCargando from '../../components/AdminCargando';
+import React, { Component } from "react";
+import { URL_BACKEND } from "./../../../../environments/environments";
+import AdminCargando from "../../components/AdminCargando";
 export default class AdminPabellones extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       pabellones: [],
       cargando: true
-    }
+    };
   }
 
   getPabellones = async () => {
@@ -18,47 +17,66 @@ export default class AdminPabellones extends Component {
       this.setState({
         cargando: false,
         pabellones: json.contenido
-      })
+      });
     }
-  }
+  };
 
   componentDidMount() {
     this.getPabellones();
   }
 
   render() {
-
     if (this.state.cargando) {
-      return (<AdminCargando />)
+      return <AdminCargando />;
     }
     return (
       <div id="content">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
-            <button type="button" id="sidebarCollapse"
-              class="btn btn-info" onClick={() => {
-                this.props.toggleAbierto()
-              }}>
+            <button
+              type="button"
+              id="sidebarCollapse"
+              class="btn btn-info"
+              onClick={() => {
+                this.props.toggleAbierto();
+              }}
+            >
               <i class="fas fa-align-left"></i>
               <span>Toggle Sidebar</span>
             </button>
-            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button
+              class="btn btn-dark d-inline-block d-lg-none ml-auto"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <i class="fas fa-align-justify"></i>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Page</a>
+                  <a class="nav-link" href="#">
+                    Page
+                  </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Page</a>
+                  <a class="nav-link" href="#">
+                    Page
+                  </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Page</a>
+                  <a class="nav-link" href="#">
+                    Page
+                  </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Page</a>
+                  <a class="nav-link" href="#">
+                    Page
+                  </a>
                 </li>
               </ul>
             </div>
@@ -66,7 +84,30 @@ export default class AdminPabellones extends Component {
         </nav>
 
         <h2>Pabellones</h2>
-
+        <div className="row">
+          <div class="col-12">
+            <table>
+              <thead>
+                <tr>
+                  <th>N°</th>
+                  <th>Nombre Pabellón</th>
+                  <th>Cant. Ambientes</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.pabellones.map((pab, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{index}</td>
+                      <td>{pab.pab_nom}</td>
+                      <td>{pab.ambientes.length}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
