2e0e1aca789376399698a31edbb39efb07875316
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-02-13 20:02:09 -0500

    avance

--- Semana16/ambientes/frontend/src/modulos/admin/screens/reservas/AdminReservas.js
+++ Semana16/ambientes/frontend/src/modulos/admin/screens/reservas/AdminReservas.js
@@ -3,7 +3,76 @@ import React, { Component } from 'react';
 export default class AdminReserva extends Component {
   render() {
     return (
-      <div> textInComponent </div>
+        <div id="content">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <button
+              type="button"
+              id="sidebarCollapse"
+              className="btn btn-info"
+              onClick={() => {
+                this.props.toggleAbierto();
+              }}
+            >
+              <i className="fas fa-align-left"></i>
+              <span>Toggle Sidebar</span>
+            </button>
+            <button
+              className="btn btn-dark d-inline-block d-lg-none ml-auto"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i className="fas fa-align-justify"></i>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="nav navbar-nav ml-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">
+                    Page
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Page
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Page
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Page
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <h2>Reservas</h2>
+        <button
+          type="button"
+          className="btn btn-primary" data-target="#modalCrear" data-toggle="modal">
+          Agregar Reserva
+                </button>
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card-body shadow">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
     );
   }
 }
