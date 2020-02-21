a1125de068c533744bf4d3c0dbb0354856daa037
Osmar Montesinos <osmarmontes50@gmail.com>
2020-02-20 19:14:43 -0500

    avance 2

--- Semana21/chat/back/index.js
+++ Semana21/chat/back/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const socketio = require('socket.io');
 //socket.io para la comunicación require un servidor http, este ya se encuentra en node
 const http = require('http');
+const router = require('./routes');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(router);
 
 const server = http.createServer(app);
+const io = socketio(server);
 
-
+server.listen(PORT, () => {
+    console.log(`El servidor de sockets esta funcionando en: ${PORT}`);
+})
